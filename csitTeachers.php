5ebbcc6d8c010c7902b99379f924fc46272cf483
Matthew Wing <wingm1@montclair.edu>
2018-12-09 23:59:42 -0500

    files added

---
+++ csitTeachers.php
@@ -0,0 +1,23 @@
+<?php
+    $option = $_POST['professors'];
+    $jing = 'Jingfu Jenq';
+    $boxiang = 'Boxiang Dong';
+    $dolezal = 'Herman Dolezal';
+    $dawei = 'Dawei Li';
+    $hongbo = 'Hongbo Zhou';
+        if($option == $jing){
+            echo '<script>location.assign("https://www.montclair.edu/profilepages/view_profile.php?username=jenqj")</script>';
+        }
+        if($option == $boxiang){
+            echo '<script>location.assign("https://www.montclair.edu/profilepages/view_profile.php?username=dongb")</script>';
+        }
+        if($option == $dolezal){
+            echo '<script>location.assign("https://www.montclair.edu/profilepages/view_profile.php?username=dolezalh")</script>';
+        }
+        if($option == $dawei){
+            echo '<script><location.assign("https://www.montclair.edu/profilepages/view_profile.php?username=lida")/script>';
+        }
+        if($option == $hongbo){
+            echo '<script>location.assign("http://cs.montclair.edu/people/Zhou.html")</script>';
+        }
+?>
