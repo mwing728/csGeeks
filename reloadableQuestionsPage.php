4b809e9e480cd008efea616527e7550a5f0b082a
AlexEspo <45057180+AlexEspo@users.noreply.github.com>
2018-12-09 23:21:10 -0500

    Add files via upload

---
+++ reloadableQuestionsPage.php
@@ -0,0 +1,97 @@
+<html>
+    <head>
+        <title>CS Geek Question Forum</title>
+        <link rel = "stylesheet" type= "text/css" href = "styleQuestions.css">
+        <script type = "text/javascript">
+            function pushToPreviousQuestions(){
+                location.assign("reloadableQuestionsPage.php");
+            }
+            function pushToAnswerQuestions(){
+                location.assign("answerQuestions.php");
+            }
+            function pushToInsertingQuestions(){
+                location.assign("insertingQuestions.php");
+            }
+        </script>
+    </head>
+
+    <body>
+        <header>
+            <div class = "nav-bar">
+                <ul class = "menu">
+                    <li><a href = "home.html"> Home</a></li>
+                    <li><a href = "csit.html"> CSIT Classes</a></li>
+                    <li><a href = "AlexEsposito.html"> Logout</a></li>
+                </ul>
+            </div>
+
+            <img src = "avatar.jpg" class = "avatar">
+
+        <div class = "q_area">
+            <h2>Post Your Questions Here!</h2>
+            <p>Ask questions on any CSIT related topic:</p>
+            <form action = "insertingQuestions.php" method = "post">
+                <textarea name = "message" rows="5" cols="30" placeholder="Enter question here...">
+                </textarea><br><br>
+                <button type="submit" value="Ask!">Ask Question</button><br>
+            </form>
+                <button type = "submit" value = "Refresh" onclick = pushToPreviousQuestions();>Refresh</button>
+        </div>
+        <div class = "a_area">
+            <form action = "answerQuestions.php" method ="post">
+                <h2>Answer Questions Here!</h2>
+                <p>Answer any previously asked questions:</p>
+                <textarea name = "answer" rows="5" cols="30" placeholder="Answer question here...">
+                </textarea><br>
+                <p>Question Number:</p>
+                <input type = "number" size = "32" name = "questionID" placeholder="Enter question #..."><br><br>
+                <button type="submit" value="Answer Question!">Answer Question</button>
+            </form>
+        </div>
+
+        <h3><i>View Previously Asked Questions:</i></h3>
+        <h4><i>View Previously Asnswered Questions:</i></h4>
+
+        <div class = "q_box">
+            <?php
+                try {
+                    $conn = new PDO('mysql:host=127.0.0.1;dbname=matthewwingdatabase', 'matthewwing', 'matthewwingpass');
+                }
+                catch(PDOException $e){
+                    echo $e->getMessage();
+                }
+                $questionNumber = 0;
+                $query = "SELECT * from questions";
+                $result = $conn->prepare($query);
+                $result->execute();
+                $answer = $result->fetch();
+                foreach($conn->query($query) as $row){
+                    echo "Question " . $row['ID'] . ":" . $row['question'];
+                    echo "<br>";
+                    echo "<br>";
+                    $questionNumber = $row['ID'];
+                }
+            ?>
+        </div>
+        <div class = "a_box">
+            <?php
+                try {
+                    $conn = new PDO('mysql:host=127.0.0.1;dbname=matthewwingdatabase', 'matthewwing', 'matthewwingpass');
+                }
+                catch(PDOException $e){
+                    echo $e->getMessage();
+                }
+                $query2 = "SELECT * from questions";
+                $result2 = $conn->prepare($query2);
+                $result->execute();
+                $answer2 = $result2->fetch();
+                foreach($conn->query($query2)as $row){
+                    echo "Answer " . $row['ID'] . ":" . $row['answer'];
+                    echo "<br>";
+                    echo "<br>";
+                }
+            ?>
+        </div>
+        </header>
+    </body>
+</html>
